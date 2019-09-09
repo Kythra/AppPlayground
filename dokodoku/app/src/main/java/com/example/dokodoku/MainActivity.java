a9dab9e116c98550eed04cdf4d9772904556a262
kythra <katja.stumpp@gmx.de>
2019-09-09 22:48:08 +0200

    remove useless files

---
+++ dokodoku/app/src/main/java/com/example/dokodoku/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.dokodoku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
